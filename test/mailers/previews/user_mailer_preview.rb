8859104fdf0321f9e9da4ef41a3ebf89fe7aad13
Ryan Kubow <ryanpkubow@gmail.com>
2021-09-20 22:22:39 +0000

    Add password reset

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -2,7 +2,7 @@
 class UserMailerPreview < ActionMailer::Preview
 
   # Preview this email at
-  # https://us-east-2.console.aws.amazon.com/rails/mailers/user_mailer/account_activation <-- Does not seem to work for some reason
+  # http://localhost:3000/rails/mailers/user_mailer/account_activation
   def account_activation
     user = User.first
     user.activation_token = User.new_token
@@ -12,6 +12,8 @@ class UserMailerPreview < ActionMailer::Preview
   # Preview this email at
   # http://localhost:3000/rails/mailers/user_mailer/password_reset
   def password_reset
-    UserMailer.password_reset
+    user = User.first
+    user.reset_token = User.new_token
+    UserMailer.password_reset(user)
   end
 end

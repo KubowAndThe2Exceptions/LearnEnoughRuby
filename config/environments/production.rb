929d6fb7c2c667e28fa7dc2f2f567df02134a514
Ryan Kubow <ryanpkubow@gmail.com>
2021-09-22 02:34:54 +0000

    Add user microposts

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,8 +37,8 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
-  # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :local
+    # Store uploaded files on Amazon AWS.
+  config.active_storage.service = :amazon
 
 
   config.action_mailer.raise_delivery_errors = true
